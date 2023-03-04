1e0dcd3284c9917dd96857af39e28d9ea9e51c0f
4c <4c@labinfo1-21>
2023-03-04 09:17:46 +0100

    Aggiunta del Costruttore e metodi Set e Get

--- Team/src/Test.java
+++ Team/src/Test.java
@@ -1,4 +1,15 @@
 
 public class Test {
  private String a;
+ 	public Test(String a) {
+ 		this.a=a;
+ 	}
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
+ 	
+ 	
 }
